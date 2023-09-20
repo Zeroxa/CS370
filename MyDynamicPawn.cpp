7058dd235df3d3777cb19e4fc90521a7e89e8181
Zeroxa <2991971hz@gmail.com>
2023-09-20 13:46:34 -0700

    Update MyDynamicPawn.cpp

--- MyDynamicPawn.cpp
+++ MyDynamicPawn.cpp
@@ -1,6 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
 #include "MyDynamicPawn.h"
-#include "UObject/ConstructorHelpers.h"
-#include "Components/StaticMeshComponent.h"
 
 // Sets default values
 AMyDynamicPawn::AMyDynamicPawn()
@@ -10,19 +11,21 @@ AMyDynamicPawn::AMyDynamicPawn()
 
 	AutoPossessPlayer = EAutoReceiveInput::Player0;
 
-	OurVisibleComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("OurVisibleComponent"));
-	RootComponent = OurVisibleComponent; // Set the Mesh component as the RootComponent
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+	
+	OurVisibleComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("VisualMesh"));
+	OurVisibleComponent->SetupAttachment(RootComponent);
 
 	OurCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("OurCamera"));
-	OurCamera->SetupAttachment(RootComponent); // Attach the camera component to our root component (which is the mesh)
-	OurCamera->SetRelativeLocation(FVector(-500.0f, 0.0f, 350.0f)); // Set a default position for the camera
-	OurCamera->SetRelativeRotation(FRotator(-35.0f, 0.0f, 0.0f)); // Set a default rotation for the camera
+	OurCamera -> SetupAttachment(RootComponent);
 
-	OurVisibleComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("OurVisibleComponent"));
-	RootComponent = OurVisibleComponent;
+	OurCamera->SetRelativeLocation(FVector(-500.0f, 0.0f, 350.0f));
+	OurCamera->SetRelativeRotation(FRotator(-35.0f, 0.0f, 0.0f));
 
-	OurCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("OurCamera"));
-	OurCamera->SetupAttachment(RootComponent);
+	bGrowing = true;
+	bZooming = true;
+	zoomRate = 40.0f;
+
 }
 
 // Called when the game starts or when spawned
@@ -37,7 +40,57 @@ void AMyDynamicPawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	float CurrentScale = OurVisibleComponent->GetComponentScale().X;
+
+	if (bGrowing)
+	{
+		CurrentScale += DeltaTime;
+	}
+	else
+	{
+		CurrentScale -= (DeltaTime * 0.5f);
+	}
+
+	CurrentScale = FMath::Clamp(CurrentScale, 1.0f, 3.0f);
+	OurVisibleComponent->SetWorldScale3D(FVector(CurrentScale));
+
+	FVector camLocation = OurCamera->GetRelativeTransform().GetLocation();
+	FVector objLocation = OurVisibleComponent->GetRelativeTransform().GetLocation();
+	FVector direction = (camLocation - objLocation).GetSafeNormal();
+
+	float zoomDistance = zoomRate * DeltaTime;
+    
+	float distance = (camLocation - objLocation).Size();
+	float minDistance = 250.0f;
+	float maxDistance = 1000.0f;
+	distance = FMath::Clamp(distance, minDistance, maxDistance);
+
+	// Check if camera is too close or too far before updating its location
+	if ((bZooming && distance > minDistance) || (!bZooming && distance < maxDistance))
+	{
+		if (bZooming)
+		{
+			camLocation -= direction * zoomDistance; // Zoom in
+		}
+		else
+		{
+			camLocation += direction * zoomDistance; // Zoom out
+		}
+	}
+
+	OurCamera->SetRelativeLocation(camLocation);
+    
+	// Display messages
+	FString DebugStringGrowing = "Growing: " + FString(bGrowing ? "True" : "False") + ", Scale: " + FString::SanitizeFloat(CurrentScale);
+	GEngine->AddOnScreenDebugMessage(1, 5.0f, FColor::Yellow, DebugStringGrowing);
+
+	FString DebugStringZooming = "Zooming In: " + FString(bZooming ? "True" : "False") + ", Zoom Rate: " + FString::SanitizeFloat(zoomRate);
+	GEngine->AddOnScreenDebugMessage(2, 5.0f, FColor::Green, DebugStringZooming);
+
+	FString DebugStringDistance = "Distance from Actor: " + FString::SanitizeFloat(distance);
+	GEngine->AddOnScreenDebugMessage(3, 5.0f, FColor::White, DebugStringDistance);
 }
+
 
 // Called to bind functionality to input
 void AMyDynamicPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
@@ -45,3 +98,4 @@ void AMyDynamicPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputCompo
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 }
+
