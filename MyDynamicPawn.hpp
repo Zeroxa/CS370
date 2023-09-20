754902276646fe8595de9f739e47f2c7eecefe39
Zeroxa <2991971hz@gmail.com>
2023-09-20 13:46:59 -0700

    Update MyDynamicPawn.hpp

--- MyDynamicPawn.hpp
+++ MyDynamicPawn.hpp
@@ -1,9 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "Camera/CameraComponent.h"
-#include "MyDynamicPawn.generated.h" 
+#include "MyDynamicPawn.generated.h"
 
 UCLASS()
 class PLAYINGWITHDELTATIME_API AMyDynamicPawn : public APawn
@@ -19,6 +21,16 @@ public:
 
 	UPROPERTY(EditAnywhere, Category = "MyCamera")
 	UCameraComponent* OurCamera;
+
+	UPROPERTY(EditAnywhere, Category = "Growth")
+	bool bGrowing;
+
+	UPROPERTY(EditAnywhere, Category = "CameraZoom")
+	bool bZooming;
+
+	UPROPERTY(EditAnywhere, Category = "CameraZoom", meta = (UIMin = "0.0", UIMax = "100.0"))
+	float zoomRate;
+
 
 protected:
 	// Called when the game starts or when spawned
